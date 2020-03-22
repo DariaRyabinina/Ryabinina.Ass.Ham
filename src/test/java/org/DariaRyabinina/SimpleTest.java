a452faf19750723b33c4d314f8c6343827f1d987
Daria <ryabinina@i-teco.ru>
2020-03-22 15:22:26 +0300

    ch.4

--- src/test/java/org/DariaRyabinina/SimpleTest.java
+++ src/test/java/org/DariaRyabinina/SimpleTest.java
@@ -4,11 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -17,7 +16,7 @@ public class SimpleTest {
     public void simpleTest1() {
         BigDecimal weight = new BigDecimal(60.2).setScale(3, BigDecimal.ROUND_HALF_UP);
         List<String> jobs = Arrays.asList("Художник", "Поэт", "Музыкант");
-        Person vasia= new Person("Василий", 45, weight, jobs);
+        Person vasia = new Person("Василий", 45, weight, jobs);
         Assertions.assertThat(vasia)
                 .as("Вассилий")
                 .hasFieldOrProperty("name")
@@ -26,6 +25,7 @@ public class SimpleTest {
         Assertions.assertThat(vasia.getJobs())
                 .containsAnyOf("Поэт");
     }
+
     @Test(testName = "Hamcrest")
     public void simpleTest2() {
         BigDecimal weight = new BigDecimal(60.2).setScale(3, BigDecimal.ROUND_HALF_UP);
